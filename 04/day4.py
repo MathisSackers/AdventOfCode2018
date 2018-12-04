fb8d8c75e077cb137c413a325aa5f1c32c222314
Mathis Sackers <m.sackers@student.ru.nl>
2018-12-04 17:14:31 +0100

    day 4 part 1

---
+++ 04/day4.py
@@ -0,0 +1,47 @@
+import time
+
+
+def minutes(line):
+    return int(line[line.index(':')+1:line.index(']')])
+
+
+# part 1
+start = time.time()
+with open('./input.txt', 'r') as f:
+    lines = sorted(f.readlines())
+    # find the most sleepy guard
+    sleeptimes = {}
+    guard_id = 'none'
+    last_time = 0
+    max_id = 'none'
+    max_time = 0
+    for line in lines:
+        if '#' in line:  # Guard #x begins shift
+            guard_id = line[line.index('#')+1:line.index('b')-1]
+            if guard_id not in sleeptimes:
+                sleeptimes[guard_id] = 0
+        elif 'w' in line:  # wakes up
+            sleeptimes[guard_id] += minutes(line) - last_time
+            if sleeptimes[guard_id] > max_time:
+                max_time = sleeptimes[guard_id]
+                max_id = guard_id
+        else:  # falls asleep
+            last_time = minutes(line)
+    #  find the minute
+    counting = False
+    counter = [0 for x in range(60)]
+    for line in lines:
+        if '#' in line:  # Guard #x begins shift
+            if line[line.index('#')+1:line.index('b')-1] == max_id:
+                counting = True
+            else:
+                counting = False
+        elif counting:
+            if 'w' in line:  # wakes up
+                for x in range(last_time, minutes(line)):
+                    counter[x] += 1
+            else:  # falls asleep
+                last_time = minutes(line)
+    print('Part 1:', counter.index(max(counter)) * int(max_id))
+    end = time.time()
+    print(end - start)
