c9f9be1a8b9e24db468ed0eedf737b8d9bc3a6fd
Mathis Sackers <m.sackers@student.ru.nl>
2018-12-07 23:51:16 +0100

    day 6 part 1

---
+++ 06/day6.py
@@ -0,0 +1,45 @@
+import time
+import numpy as np
+
+
+# part 1
+with open('./input.txt', 'r') as f:
+    start = time.time()
+    lines = f.readlines()
+    points = [[int(line[:line.index(',')]), int(line[line.index(' ') + 1:])] for line in lines]
+    top, bottom = points[0][1], points[0][1]
+    left, right = points[0][0], points[0][0]
+    grid = np.zeros(shape=(500, 500))  # pretty arbitrary
+    infinites = []
+    for x in range(500):
+        for y in range(500):
+            pid = -1
+            pdist = 500*500
+            for point in points:
+                dist = abs(point[0] - x) + abs(point[1] - y)
+                if dist < pdist:
+                    pid = points.index(point)
+                    pdist = dist
+                elif dist == pdist:  # equal distance
+                    pid = -1
+            grid[y][x] = pid
+            if min(x, y) == 0 or max(x, y) == 499:  # naive
+                if pid not in infinites:
+                    infinites.append(pid)
+    unique, counts = np.unique(grid, return_counts=True)
+    unique, counts = [int(x) for x in list(unique)], [int(x) for x in list(counts)]
+    print(counts)
+    # discount invalids
+    for infinite in infinites:
+        counts[unique.index(infinite)] = -1
+    counts[unique.index(-1)] = -1
+    print(counts)
+    print('Part 1:', max(counts))
+    end = time.time()
+    print(end - start)
+    # part 2
+    start = time.time()
+    # TODO
+    print('Part 2:',)
+    end = time.time()
+    print(end - start)
